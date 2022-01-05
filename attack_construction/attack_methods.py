89f964be24d24ea00d0b32f538526ea3b626d5ea
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-01-06 00:09:28 +0700

    fix

--- attack_construction/attack_methods.py
+++ attack_construction/attack_methods.py
@@ -60,8 +60,8 @@ def training_step(model, patch, augmentations, images, labels, loss, device, gra
 
     predict = model(attacked_images)
 
-    print(attacked_image[0].requared_grad)
-    print(predict[0].requared_grad)
+    print(attacked_image[0].requares_grad)
+    print(predict[0].requares_grad)
 
     costs = loss(predict, patch, device)
 
