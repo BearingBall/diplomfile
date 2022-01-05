9fbf6ce84cc729c86a4cf9fbc2ece19ec7eadc47
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-01-05 23:19:20 +0700

    fix

--- attack_construction/attack_methods.py
+++ attack_construction/attack_methods.py
@@ -62,7 +62,7 @@ def training_step(model, patch, augmentations, images, labels, loss, device, gra
 
     costs = loss(predict, patch, device)
 
-    grad = torch.autograd.grad(costs, patch, retain_graph=False, create_graph=False, allow_unused=True)[0]
+    grad = torch.autograd.grad(np.sum(costs), [patch], retain_graph=False, create_graph=False, allow_unused=True)[0]
 
     print("grad ", grad.shape, ' ', np.sum(grad.detach().cpu().numpy()))
 
