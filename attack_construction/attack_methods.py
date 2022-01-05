b325efeb3fe52007e15f9d8e4c43d2574579b82c
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-01-05 18:55:39 +0700

    fix

--- attack_construction/attack_methods.py
+++ attack_construction/attack_methods.py
@@ -64,9 +64,6 @@ def training_step(model, patch, augmentations, images, labels, loss, device, gra
 
     grad = torch.autograd.grad(costs, patch, retain_graph=False, create_graph=False, allow_unused=True)[0]
 
-    print(patch.shape)
-    print(grad.shape)
-
     if grad is not None:
         patch = patch + grad_rate * grad.sign()
 
