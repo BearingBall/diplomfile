23123a7c1c5c795fb29263f7eee95fcb1731368b
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-01-06 13:00:26 +0700

    test

--- attack_construction/attack_methods.py
+++ attack_construction/attack_methods.py
@@ -58,7 +58,7 @@ def training_step(model, patch, augmentations, images, labels, loss, device, gra
         for label in labels[i-1]:
             attacked_image = insert_patch(attacked_image, augmented_patch, label, 0.3, device)
 
-    predict = model([patch])
+    predict = model(attacked_image)
 
     costs = loss(predict, patch, device)
 
