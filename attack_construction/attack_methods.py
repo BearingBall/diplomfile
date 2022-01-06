22a7d6fa2ea7a96e087741c10a6bcf178fc84a01
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-01-06 17:53:26 +0700

    test

--- attack_construction/attack_methods.py
+++ attack_construction/attack_methods.py
@@ -65,12 +65,7 @@ def training_step(model, patch, augmentations, images, labels, loss, device, gra
 
     costs = loss(predict, patch, device)
 
-    for am in attacked_images:
-        print(am.required_grad)
-
-    print(len(attacked_images), attacked_images[0].shape)
-
-    grad = torch.autograd.grad(outputs=sum(sum(sum(sum(attacked_images)))), inputs=patch, retain_graph=True, create_graph=True, allow_unused=True)[0]
+    grad = torch.autograd.grad(outputs=sum(sum(sum(attacked_images[0]))), inputs=patch, retain_graph=True, create_graph=True, allow_unused=True)[0]
 
     patch = patch.detach()
 
