5f88bc12fc8fc7ecdcc870b38eba0e2afe91b5c3
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-02-13 23:19:22 +0700

    fix

--- attack_construction/attack_methods.py
+++ attack_construction/attack_methods.py
@@ -76,7 +76,9 @@ def training_step(model, patch, augmentations, images, labels, loss, device, opt
             for j in range(len(labels[i])):
                 if (labels[i][j][2] != 0 and labels[i][j][3] !=0):
                     targets[i]['boxes'].append(labels[i][j])
-            targets[i]['boxes'] = torch.tensor(targets[i]['boxes'])
+            #targets[i]['boxes'] = torch.tensor(targets[i]['boxes'])
+
+        print(targets)
 
         predict = model(attacked_images, targets)
 
