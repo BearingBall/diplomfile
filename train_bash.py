6112e0209bfdb707e35a26bf8606e2f72e842bdc
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-03-18 01:08:26 +0700

    fix

--- train_bash.py
+++ train_bash.py
@@ -89,6 +89,8 @@ def main():
         torchvision.transforms.RandomRotation(degrees=(-5, 5)),
     ])
 
+    augmentations = None
+
     loss_function = partial(adversarial_loss_function_batch, tv_scale=args.tv_scale)
 
     for epoch in range(epoches):
