cceb04b4bca5d2740a8a592d36a20a06de6d66b1
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-04-04 01:24:03 +0700

    fix

--- train_bash.py
+++ train_bash.py
@@ -94,7 +94,7 @@ def main():
         num_workers=10
     )
 
-    annotation_file="../../annotations_trainval2017/annotations/instances_val2017.json"
+    annotation_file="../annotations_trainval2017/annotations/instances_val2017.json"
 
     patch = attack_methods.generate_random_patch()
     patch = patch.cuda()
@@ -120,7 +120,7 @@ def main():
 
     for epoch in range(epoches):
         train(attack_module, small_train_loader, augmentations, optimizer, writer, loss_function)
-        mAPs = validate(attack_module, small_val_loader, augmentations, annotation_file)
+        mAPs = validate(attack_module, small_train_loader, augmentations, annotation_file)
         for i, mAP in enumerate(mAPs):
             writer.add_scalar('mAP, model: ' + str(i), mAP, epoch)
 
