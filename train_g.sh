47b7d55b5158a52841c35712b2f44e95d31e9774
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-05-12 11:57:55 +0700

    fix

--- train_g.sh
+++ train_g.sh
@@ -1,6 +1,6 @@
     # environment variables
 dist='torch.distributed.launch'
-python='python3'
+python='torchrun'
 executable='train_bash.py'
 
 devices='0'
@@ -14,7 +14,7 @@ PATH_TO_COCO_ANN=$PATH_TO_COCO'annotations_trainval2017/annotations/'
 
 for i in 1
 do
-    CUDA_VISIBLE_DEVICES=$devices $python -m $dist $executable \
+    CUDA_VISIBLE_DEVICES=$devices $python $executable \
         --val_labels '../annotation_cutted' \
         --val_part 1 \
         --batch_size $BATCH_SIZE \
