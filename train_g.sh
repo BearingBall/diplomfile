0a60ab51ae1e317e3d0b6e8756c2e9e4140da0fe
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-05-12 12:15:25 +0700

    fix

--- train_g.sh
+++ train_g.sh
@@ -11,10 +11,12 @@ TV_SCALE=0.001
 PATH_TO_COCO='/home/goncharenko/4tb_folder_first/data/coco_orig/'
 PATH_TO_COCO_ANN=$PATH_TO_COCO'annotations_trainval2017/annotations/'
 
+NUM_TRAINERS=2
+
 
 for i in 1
 do
-    CUDA_VISIBLE_DEVICES=$devices $python $executable \
+    CUDA_VISIBLE_DEVICES=$devices $python --nproc_per_node=$NUM_TRAINERS $executable \
         --val_labels '../annotation_cutted' \
         --val_part 1 \
         --batch_size $BATCH_SIZE \
