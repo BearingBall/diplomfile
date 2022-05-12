10f667dbf5bf2337cb2d637962c9c934b526fc8e
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-05-12 22:15:01 +0700

    fix

--- train_g.sh
+++ train_g.sh
@@ -22,7 +22,7 @@ do
         --batch_size $BATCH_SIZE \
         --epochs 20 \
         --device 1 \
-	    --experiment_dir 'gtrain_12_05_2022' \
+	    --experiment_dir 'gtrain_12_05_2022_2' \
 	    --tv_scale $TV_SCALE \
 	
 done
