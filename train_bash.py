587d8c9259ee52986a26be996807e876a599ef35
BearingBall <74752877+BearingBall@users.noreply.github.com>
2022-05-12 22:36:43 +0700

    fix

--- train_bash.py
+++ train_bash.py
@@ -79,15 +79,15 @@ def main():
     )
 
     small_train_loader = torch.utils.data.DataLoader(
-        dataset=dataset,
+        dataset=torch.utils.data.Subset(dataset, range(0, 100)),
         batch_size=batch_size,
         num_workers=10,
         sampler=DistributedSampler(
-                dataset=dataset)
+                dataset=torch.utils.data.Subset(dataset, range(0, 100)))
     )
 
     small_val_loader = torch.utils.data.DataLoader(
-        dataset=torch.utils.data.Subset(dataset_val, range(0, int(len(dataset_val) * 0.01))),
+        dataset=torch.utils.data.Subset(dataset_val, range(0, 100)),
         batch_size=30,
         num_workers=10
     )
@@ -122,8 +122,8 @@ def main():
     writer = SummaryWriter(log_dir=experiment_dir.as_posix())
 
     for epoch in range(epoches):
-        train(attack_module, train_loader, augmentations, optimizer, writer, loss_function)
-        mAPs = validate(attack_module, val_loader, augmentations, annotation_file, local_rank)
+        train(attack_module, small_train_loader, augmentations, optimizer, writer, loss_function)
+        mAPs = validate(attack_module, small_val_loader, augmentations, annotation_file, local_rank)
 
         if (local_rank == 0):
             print("mAPs: ", mAPs)
